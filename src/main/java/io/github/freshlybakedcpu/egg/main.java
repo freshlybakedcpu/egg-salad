588c73a16d9d74e74bd8dcf1cb1948d1f9cdac3a
freshlybakedcpu <btl2202@iolani.org>
2020-11-16 13:04:04 -1000

    Added OpenNLP as dependency

--- src/main/java/io/github/freshlybakedcpu/egg/main.java
+++ src/main/java/io/github/freshlybakedcpu/egg/main.java
@@ -1,5 +1,13 @@
 package io.github.freshlybakedcpu.egg;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+
 public class main {
     public static void main(String[] args) {
 
